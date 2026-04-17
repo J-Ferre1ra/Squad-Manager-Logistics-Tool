b732ba1b3f7e49957162719cf38384f72b462f13
João Henrique Ferreira <157930541+J-Ferre1ra@users.noreply.github.com>
2026-04-16 21:40:03 -0300

    feat: implementa lógica de cálculo de média com reduce

--- src/app.js
+++ src/app.js
@@ -3,7 +3,25 @@ const dadosBrutos = [
     { nome: "Cadete", notas: [6, 5, 8], idade: 22 },
     { nome: "Igor", notas: [10, 9, 9], idade: 19 },
     { nome: "Vitor", notas: [4, 5, 3], idade: 20 },
-    { nome: "Atleta Teste", notas: [0], idade: 99 } // Esse deve ser removido pelo pop()
+    { nome: "Atleta Teste", notas: [0], idade: 99 } 
 ];
 
-const CUSTO_VIAGEM = 500; // Valor total a ser rateado entre os titulares
+const CUSTO_VIAGEM = 500; 
+
+dadosBrutos.unshift("Goleiro")
+dadosBrutos.pop
+
+const calculoPerformance = dadosBrutos.map(atleta => {
+    
+    const media  = atleta.notas.reduce((acc, atleta, index) => acc + this.notas[index], 0)
+
+    if (atleta.notas.length > 0) {
+        const reusltadoMedia = media / atleta.notas.length
+    }
+    
+    return {
+        nome: atleta.nome,
+        media: reusltadoMedia
+    }
+
+})
